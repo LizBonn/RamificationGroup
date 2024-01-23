5aae93624f1160364131630b10582c54479fe7d8
jjdishere <emailboxofjjd@163.com>
2024-01-24 02:17:14 +0800

    redefine CVF

--- RamificationGroup/Definition/CompleteValuationRing.lean
+++ RamificationGroup/Definition/CompleteValuationRing.lean
@@ -1,26 +1,45 @@
 import RamificationGroup.Definition.MissingPiecesOfMathlib
+import Mathlib.RingTheory.Valuation.ValuationSubring
 /-!
 # Complete Valuation Ring and Complete Discrete Valuation Ring
 
 In this file, we define
 -/
 
+
+
 open Classical Uniformity Topology Filter Set
+open ValuationTopology
 -- noncomputable
 -- section
 
-class CompleteValuationRing (R : Type*) [CommRing R] [IsDomain R] extends ValuationRing R, UniformSpace R, CompleteSpace R, TopologicalRing R where
-  val_top : sorry
+-- variable  (R : Type*) [CommRing R] [IsDomain R] [ValuationRing R]
+
+class CompleteValuationRing (R : Type*) [CommRing R] [IsDomain R] extends ValuationRing R, CompleteSpace R, OrderTopology R
+-- Note that this definition automatically ensures the topology is induced by valuation by `OrderTopology` and instances of `Order R`, `Topology R` from `ValuationRing R`
+
+--`Maybe not so bad, A CDVF can be defined as the valued field whose valuation ring is a CDVR`
+class CompleteDiscreteValuationRing (R : Type*) [CommRing R] [IsDomain R] extends DiscreteValuationRing R, CompleteValuationRing R
+
+-- Or `ValuedField?`
+-- Now I believe that the valuation structure on a field is never canonical, so its better to store information of the valuation ring.
+-- use `mathcal{O}`?
+class CompleteValuationField (K : Type*) [Field K] where
+  O : ValuationSubring K
+  valuation_subring_is_complete : CompleteValuationRing O
+
+class CompleteDiscreteValuationField (K : Type*) [Field K] extends CompleteValuationField K where
+  valuation_subring_is_dvr : DiscreteValuationRing O
+
+
+
 
 /-
--- `Bad Choice, not compatible with CDVF`
-class CompleteDiscreteValuationRing (R : Type*) [CommRing R] [IsDomain R] extends DiscreteValuationRing R, CompleteValuationRing R
--/
-
 class CompleteDiscreteValuationRing (R : Type*) [CommRing R] [IsDomain R] extends CompleteValuationRing R where
   is_discrete : sorry
 
--- these two definition is not so necessary
+-- Maybe these two definition is not so necessary
 class CompleteValuationField (R : Type*) [Field R] extends CompleteValuationRing R
 
 class CompleteDiscreteValuationField (R : Type*) [Field R] extends CompleteValuationRing R
+-/
--- RamificationGroup/Definition/MissingPiecesOfMathlib.lean
+++ RamificationGroup/Definition/MissingPiecesOfMathlib.lean
@@ -2,21 +2,63 @@ import Mathlib.RingTheory.Valuation.ValuationRing
 import Mathlib.RingTheory.DiscreteValuationRing.Basic
 import Mathlib.Topology.UniformSpace.Cauchy
 import Mathlib.Topology.Algebra.Ring.Basic
+import Mathlib.Topology.Order.Basic
+import Mathlib.Topology.Algebra.Valuation
 /-!
 # Missing Pieces of Mathlib
 
 In this file, we collect missing theorems, instances as prequisite of this project. Theorems in this file should be added to mathlib file scatterly into each file.
 -/
 
-variable {R: Type*} [Ring R]
+variable (R: Type*)
 -- variable {Γ₀ : Type*} [LinearOrderedCommMonoidWithZero Γ₀]
 
 namespace ValuationTopology
 variable [CommRing R] [IsDomain R] [ValuationRing R]
 
-scoped instance: TopologicalSpace R := sorry
+/-- The preorder of divisibility associated to a valuation ring, i.e. `a ≤ b` if there exist `c`, such that `a * c = b`. -/
+scoped instance : Preorder R where
+ le a b := ∃ c, a * c = b
+ le_refl _ := ⟨1, mul_one _⟩
+ le_trans _ _ _ := fun ⟨u, h⟩ ⟨v, g⟩ => ⟨u * v, by rw [← g, ← h]; ring⟩
 
-scoped instance: TopologicalRing R := sorry
+/-- The topology on a valuation ring `R` is defined to be the topology associated to the preorder of divisibility.-/
+scoped instance : TopologicalSpace R := Preorder.topology R
+
+scoped instance : OrderTopology R := ⟨rfl⟩
+
+scoped instance : UniformSpace R where
+  uniformity := sorry
+  refl := sorry
+  symm := sorry
+  comp := sorry
+  isOpen_uniformity := sorry
+
+
+/-!
+-- the following is missed in `Mathlib.RingTheory.Valuation.ValuationRing`
+
+def ValuationRing.setoid : Setoid R where
+  r a b := a ≤ b ∧ b ≤ a
+  -- 2 elements is equiv if both a ≤ b and b ≤ a
+  iseqv := sorry
+
+
+def ValuationRing.ValueMonoid := Quotient (ValuationRing.setoid R) -- this is really a monoid with zero
+
+instance : LinearOrderedCommMonoidWithZero (ValuationRing.ValueMonoid R) := sorry
+
+scoped instance : Valued R (ValuationRing.ValueMonoid R) := _
+
+-- `Valued` uses Group instead of Monoid... `Maybe the correct way is to generalize mathlib's valued to monoid instead of group???`
+-/
+
+-- instance : Valued R (ValuationRing.ValueGroup R (FractionRing R)) := sorry
+-- the topology not rfl to
+
+scoped instance : TopologicalRing R := sorry
+
+
 
 end ValuationTopology
 
