a56c887ea44aebcc01e79df91bd7df485d3d821e
Junjie Bai <77053600+Akwardbro@users.noreply.github.com>
2024-02-29 00:43:25 +0800

    update some, still need to complete

--- RamificationGroup/draft.lean
+++ RamificationGroup/draft.lean
@@ -4,6 +4,9 @@ import Mathlib.RingTheory.DiscreteValuationRing.Basic
 import Mathlib.RingTheory.DedekindDomain.AdicValuation
 import Mathlib.FieldTheory.MinPoly.Basic
 import Mathlib.RingTheory.Polynomial.Eisenstein.Basic
+import Mathlib.Data.Polynomial.Basic
+import Mathlib.RingTheory.DedekindDomain.AdicValuation
+import Mathlib.RingTheory.DedekindDomain.Ideal
 
 section
 
@@ -13,7 +16,8 @@ variable {K : Type*} [Field K] [Algebra A K] [IsFractionRing A K]
 variable {L : Type*} [Field L] [Algebra K L] [Algebra A L]
 variable {B : Type*} [CommRing B] [Algebra B L] [IsIntegralClosure B A L]
                      [IsDomain B] [DiscreteValuationRing B]
+(va : IsDedekindDomain.HeightOneSpectrum A) {vb : IsDedekindDomain.HeightOneSpectrum B}
 
-theorem PROP18 (h1 : FiniteDimensional.finrank K L = (n : ℕ)) {x : B} [Irreducible x] :
-IsEisensteinAt (minpoly x K) p := by sorry
+theorem Prop18 {x : B} (h0 : Irreducible x) (h1 : (∀ x ∈ A, va.intValuation (x : A) = vb.intValuation (x : B) * (e ^ (FiniteDimensional.finrank K L)))) :
+  := by sorry
 end
