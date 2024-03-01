5bff8785fd03fac6af4f7c2a9af4d6e715d6a608
Junjie Bai <77053600+Akwardbro@users.noreply.github.com>
2024-03-01 10:24:35 +0800

    bug need to solve

--- RamificationGroup/draft.lean
+++ RamificationGroup/draft.lean
@@ -7,17 +7,22 @@ import Mathlib.RingTheory.Polynomial.Eisenstein.Basic
 import Mathlib.Data.Polynomial.Basic
 import Mathlib.RingTheory.DedekindDomain.AdicValuation
 import Mathlib.RingTheory.DedekindDomain.Ideal
+import Mathlib.RingTheory.Ideal.Operations
+import Mathlib.LinearAlgebra.Matrix.Charpoly.Basic
 
 section
+
+open Polynomial RingHom IsDedekindDomain FiniteDimensional Ideal
 
 variable {A : Type*} [CommRing A] [IsDomain A] [DiscreteValuationRing A]
 variable {p : Ideal A}
 variable {K : Type*} [Field K] [Algebra A K] [IsFractionRing A K]
 variable {L : Type*} [Field L] [Algebra K L] [Algebra A L]
-variable {B : Type*} [CommRing B] [Algebra B L] [IsIntegralClosure B A L]
+variable {B : Type*} [CommRing B] [Algebra B L] [Algebra A B] [Algebra K B] [IsIntegralClosure B A L]
                      [IsDomain B] [DiscreteValuationRing B]
-(va : IsDedekindDomain.HeightOneSpectrum A) {vb : IsDedekindDomain.HeightOneSpectrum B}
+(va : HeightOneSpectrum A) {vb : HeightOneSpectrum B}
+{f : A →+* B}
 
-theorem Prop18 {x : B} (h0 : Irreducible x) (h1 : (∀ x ∈ A, va.intValuation (x : A) = vb.intValuation (x : B) * (e ^ (FiniteDimensional.finrank K L)))) :
-  := by sorry
+theorem Prop18 {x : B} (h0 : Irreducible x) (h2 : (∀ x ∈ A, va.intValuation (x : A) = vb.intValuation (x : B) * (e ^ (finrank K L)))) :
+(ker (aeval x)) = (map C p : Ideal A[X]) := by sorry
 end
