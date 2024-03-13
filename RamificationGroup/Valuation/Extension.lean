e0cf87229624d9ced515964352b366e7610018ef
Prowler99 <leibichang58@ruc.edu.cn>
2024-03-13 17:25:55 +0800

    Update Extension.lean
    1. (important!! (for me at least)) add a section to avoid inappropriate use of variables
    2. add new lemmas

--- RamificationGroup/Valuation/Extension.lean
+++ RamificationGroup/Valuation/Extension.lean
@@ -21,6 +21,7 @@ open LocalRing Classical
 
 noncomputable section
 
+section LocalRing
 def LocalRing.ramificationIdx : ℕ := Ideal.ramificationIdx (algebraMap A B) (maximalIdeal A) (maximalIdeal B)
 
 def LocalRing.inertiaDeg : ℕ := Ideal.inertiaDeg (algebraMap A B) (maximalIdeal A) (maximalIdeal B)
@@ -28,9 +29,12 @@ def LocalRing.inertiaDeg : ℕ := Ideal.inertiaDeg (algebraMap A B) (maximalIdea
 /- WARNING : `Smul` of this `Algebra` might be incompatible -/
 instance LocalRing.instAlgebraResidue: Algebra (ResidueField A) (ResidueField B) := (ResidueField.map (algebraMap A B)).toAlgebra
 
-variable [IsDomain A] [DiscreteValuationRing A] [IsDomain B] [DiscreteValuationRing B]
+end LocalRing
+
 
 namespace ExtDVR
+
+variable [IsDomain A] [DiscreteValuationRing A] [IsDomain B] [DiscreteValuationRing B]
 
 def rankExtDVR : ℕ := (ramificationIdx A B) * (inertiaDeg A B)
 
@@ -40,16 +44,21 @@ instance instFiniteExtResidue : FiniteDimensional (ResidueField A) (ResidueField
 
 open IntermediateField Polynomial Classical
 
+theorem exists_x : ∃x : B, (ResidueField A)⟮residue B x⟯ = ⊤ := sorry
+
+theorem exists_f_of_x (x : B) : ∃f : A[X], Monic f ∧ f.map (residue A) = minpoly (ResidueField A) (residue B x) := sorry
+
+section x_and_f
+
 variable {A} {B}
 -- `x` : a lift of a primitive element of `k_B/k_A`
 -- WARNING: `hx` might not be a good statement
 variable {x : B} (hx : (ResidueField A)⟮residue B x⟯ = ⊤)
--- `f` : a monic polynomial with `A`-coeff that reduce to the minpoly of `x : k_B` over `k_A`
+-- `f` : a monic polynomial with `A`-coeff that reduces to the minpoly of `x : k_B` over `k_A`
 variable {f : A[X]} (h_monic : Monic f) (h_red : f.map (residue A) = minpoly (ResidueField A) (residue B x))
 -- `ϖ` : a uniformizer `ϖ` of `B`
 variable {ϖ : B} (hϖ : Irreducible ϖ)
 
-theorem fx_ne_0 : f.eval₂ (algebraMap A B) x ≠ 0 := sorry
 
 /-some possibly useful thms are listed below-/
 #check Field.powerBasisOfFiniteOfSeparable (ResidueField A) (ResidueField B)
@@ -58,15 +67,25 @@ theorem fx_ne_0 : f.eval₂ (algebraMap A B) x ≠ 0 := sorry
 #check Algebra.adjoin
 -- #check IsPrimitiveRoot.adjoinEquivRingOfIntegers
 -- #check IsPrimitiveRoot.integralPowerBasis
+
+/-
+lemma 3 states that `xⁱϖʲ` with finite `i j`'s form a `A`-basis of `B`.
+The next theorem is alternate and weaker version of lemma 3,
+stating that `A[x, ϖ] = B`.
+-/
+theorem lemma3_weak' : Algebra.adjoin A {x, ϖ} = ⊤ := sorry
+
+-- preparation for lemma 4
+theorem fx_ne_0 : f.eval₂ (algebraMap A B) x ≠ 0 := sorry
+
 /-
 lemma 4 states that the lifting `x` could be choose so that `f x` is a uniformizer of `B`.
 Proof:
   choose an arbitary lifting `x`.
   If `f x` is a uniformizer, it is done.
-  otherwise, `f x` lies have valuation ≥ 2, and `f (x + ϖ)` satisfy the condition.
-The following thm is the `otherwise` case.
+  otherwise, `f x` has valuation ≥ 2, and `f (x + ϖ)` is a uniformizer.
+The "otherwise" case lies below.
 -/
-
 theorem lemma4_val_ge_2 (h_fx : ¬Irreducible (f.eval₂ (algebraMap A B) x)) : Irreducible (f.eval₂ (algebraMap A B) (x + ϖ)) := sorry
 
 /-
@@ -78,14 +97,22 @@ theorem thm_val_1 (h_fx : Irreducible (f.eval₂ (algebraMap A B) x)) : Algebra.
 
 theorem thm_val_ge_2 (h_fx : ¬Irreducible (f.eval₂ (algebraMap A B) x)) : Algebra.adjoin A {x + ϖ} = ⊤ := sorry
 
-variable (A) (B)
+end x_and_f
 
-theorem exists_primitive : ∃x : B, Algebra.adjoin A {x} = ⊤ := sorry
+theorem exists_primitive : ∃x : B, Algebra.adjoin A {x} = ⊤ := by
+  rcases exists_x A B with ⟨x, _⟩
+  rcases exists_f_of_x A B x with ⟨f, _⟩
+  exact if h : Irreducible (eval₂ (algebraMap A B) x f)
+    then ⟨x, (thm_val_1 h)⟩
+    else ⟨x + (DiscreteValuationRing.exists_irreducible B).choose, (thm_val_ge_2 h)⟩
 
--- WARNING: inst conflict
+/-
+WARNING: possible inst conflict
+move higher?
+-/
 variable [NoZeroSMulDivisors A B]
 
--- #check choose_spec
+#check choose_spec (exists_primitive A B)
 -- def PowerBasisExtDVR_val_1 (h_fx : Irreducible (f.eval₂ (algebraMap A B) x)) : PowerBasis A B :=
 --   (Algebra.adjoin.powerBasis' (IsIntegral.of_finite A x) ).map (equiv_val_1' h_fx)
 
--- RamificationGroup/Valuation/SubAlgEquiv.lean
+++ RamificationGroup/Valuation/SubAlgEquiv.lean
@@ -4,8 +4,7 @@ variable {A : Type _} {B : Type _} [CommSemiring A] [Semiring B] [Algebra A B]
 variable {S : Subalgebra A B} (hS : S = ⊤)
 variable {x : B} (hx : Algebra.adjoin A {x} = ⊤)
 
-def AlgEquiv.ofTop : S ≃ₐ[A] B :=
-  { S.val with
+def AlgEquiv.ofTop : S ≃ₐ[A] B := { S.val with
     invFun := fun x ↦ ⟨x, hS.symm ▸ trivial⟩
     left_inv := fun _ ↦ rfl
     right_inv := fun _ ↦ rfl
