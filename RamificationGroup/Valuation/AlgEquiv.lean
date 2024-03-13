690aa4d61b701954a1a64b3a932e06224995c6ff
Lei Bichang <leibichang58@ruc.edu.cn>
2024-03-13 12:30:00 +0800

    Update AlgEquiv.lean

--- RamificationGroup/Valuation/AlgEquiv.lean
+++ RamificationGroup/Valuation/AlgEquiv.lean
@@ -4,4 +4,8 @@ variable (A : Type _) (B : Type _) [CommSemiring A] [Semiring B] [Algebra A B]
 
 variable {x : B} (h : Algebra.adjoin A {x} = ⊤)
 
+#synth Module A B
+
+#check LinearEquiv.ofTop
+#check LinearEquiv.ofTop (Algebra.adjoin A {x}) h
 def AlgEquiv.ofAdjoinSingleton : Algebra.adjoin A {x} ≃ₐ[A] B := sorry
