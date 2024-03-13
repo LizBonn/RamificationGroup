462f18985acde858bdccfaed70cc3f1054298198
Lei Bichang <leibichang58@ruc.edu.cn>
2024-03-13 20:25:49 +0800

    Update Extension.lean

--- RamificationGroup/Valuation/Extension.lean
+++ RamificationGroup/Valuation/Extension.lean
@@ -1,5 +1,7 @@
 /-
-TODO: put the two cases of together to give PowerBasis in general.
+TODO:
+1. prove `instFiniteExtResidue`.
+2. prove `exists_f_of_x`
 
 # of WARNINGs : 3
 
@@ -22,15 +24,23 @@ open LocalRing Classical
 noncomputable section
 
 section local_ring
-def LocalRing.ramificationIdx : ℕ := Ideal.ramificationIdx (algebraMap A B) (maximalIdeal A) (maximalIdeal B)
+namespace LocalRing
 
-def LocalRing.inertiaDeg : ℕ := Ideal.inertiaDeg (algebraMap A B) (maximalIdeal A) (maximalIdeal B)
+def ramificationIdx : ℕ := Ideal.ramificationIdx (algebraMap A B) (maximalIdeal A) (maximalIdeal B)
+
+def inertiaDeg : ℕ := Ideal.inertiaDeg (algebraMap A B) (maximalIdeal A) (maximalIdeal B)
 
 /- WARNING : `Smul` of this `Algebra` might be incompatible -/
-instance LocalRing.instAlgebraResidue: Algebra (ResidueField A) (ResidueField B) := (ResidueField.map (algebraMap A B)).toAlgebra
+instance instAlgebraResidue: Algebra (ResidueField A) (ResidueField B) := (ResidueField.map (algebraMap A B)).toAlgebra
 
+theorem residue_irreducible_eq_zero {ϖ : A} (h : Irreducible ϖ) : residue A ϖ = 0 := Ideal.Quotient.eq_zero_iff_mem.mpr ((LocalRing.mem_maximalIdeal _).mpr h.not_unit)
+
+theorem residue_eq_add_irreducible {x ϖ : A} (h : Irreducible ϖ) : residue A x = residue A (x + ϖ) := by
+  rw [RingHom.map_add, self_eq_add_right, residue_irreducible_eq_zero]
+  exact h
+
+end LocalRing
 end local_ring
-
 
 namespace ExtDVR
 
@@ -42,25 +52,27 @@ instance instFiniteExtResidue : FiniteDimensional (ResidueField A) (ResidueField
 
 open IntermediateField Polynomial Classical
 
-#check Field.exists_primitive_element
-theorem exists_x : ∃x : B, (ResidueField A)⟮residue B x⟯ = ⊤ := sorry
+theorem exists_x : ∃x : B, (ResidueField A)⟮residue B x⟯ = ⊤ := by
+  let x0 := (Field.exists_primitive_element (ResidueField A) (ResidueField B)).choose
+  use (Ideal.Quotient.mk_surjective x0).choose
+  rw [← (Field.exists_primitive_element (ResidueField A) (ResidueField B)).choose_spec]
+  congr
+  apply (Ideal.Quotient.mk_surjective x0).choose_spec
 
 #check IsIntegral.of_finite
 #check RingHom.IsIntegralElem (algebraMap A B)
 theorem exists_f_of_x (x : B) : ∃f : A[X], Monic f ∧ f.map (residue A) = minpoly (ResidueField A) (residue B x) := sorry
 
-
 variable {A} {B}
 
 section x_and_f
+
 -- `x` : a lift of a primitive element of `k_B/k_A`
--- WARNING: `hx` might not be a good statement
 variable {x : B} (hx : (ResidueField A)⟮residue B x⟯ = ⊤)
 -- `f` : a monic polynomial with `A`-coeff that reduces to the minpoly of `x : k_B` over `k_A`
 variable {f : A[X]} (h_monic : Monic f) (h_red : f.map (residue A) = minpoly (ResidueField A) (residue B x))
 -- `ϖ` : a uniformizer `ϖ` of `B`
 variable {ϖ : B} (hϖ : Irreducible ϖ)
-
 
 /-some possibly useful thms are listed below-/
 #check Algebra.adjoin.powerBasis'
@@ -70,14 +82,18 @@ variable {ϖ : B} (hϖ : Irreducible ϖ)
 -- #check IsPrimitiveRoot.integralPowerBasis
 
 /-
-lemma 3 states that `xⁱϖʲ` with finite `i j`'s form a `A`-basis of `B`.
+lemma 3 states that `xⁱϖʲ`'s with finite many `i j`'s form a `A`-basis of `B`.
 The next theorem is alternate and weaker version of lemma 3,
 stating that `A[x, ϖ] = B`.
 -/
-theorem lemma3_weak' : Algebra.adjoin A {x, ϖ} = ⊤ := sorry
+theorem lemma3_weak : Algebra.adjoin A {x, ϖ} = ⊤ := by
+  apply Algebra.eq_top_iff.mpr
+  intro y
+  sorry
 
 -- preparation for lemma 4
-theorem fx_ne_0 : f.eval₂ (algebraMap A B) x ≠ 0 := sorry
+theorem fx_ne_0 : f.eval₂ (algebraMap A B) x ≠ 0 := by
+  sorry
 
 theorem residue_primitive_of_add_uniformizer (hx : (ResidueField A)⟮residue B x⟯ = ⊤) : (ResidueField A)⟮residue B (x + ϖ)⟯ = ⊤ := sorry
 
@@ -99,10 +115,11 @@ Should use `lemma3_weak'` to prove.
 end x_and_f
 
 /-`B = A[x]` if `k_B = k_A[x]` AND `f x` is a uniformizer.
-Should use `lemma3_weak'` to prove.-/
+Should use `lemma3_weak` to prove.-/
 theorem thm_val_1 {x : B} (hx : (ResidueField A)⟮residue B x⟯ = ⊤)
     {f : A[X]} (h_fx : Irreducible (f.eval₂ (algebraMap A B) x)) :
-  Algebra.adjoin A {x} = ⊤ := sorry
+    Algebra.adjoin A {x} = ⊤ := by
+  sorry
 
 variable (A) (B)
 
