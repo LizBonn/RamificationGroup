4459a7f49462434c8da6436d4f755bf17e312c42
Lei Bichang <leibichang58@ruc.edu.cn>
2024-03-13 11:08:04 +0800

    Update Extension.lean

--- RamificationGroup/Valuation/Extension.lean
+++ RamificationGroup/Valuation/Extension.lean
@@ -1,8 +1,10 @@
--- import Mathlib.Tactic
 /-
 TODO: states `lemma 3`, i.e., express a basis of `B` over `A`.
 
 # of WARNINGs : 2
+
+does `Module.finite` implies `FiniteDimensional`?
+
 -/
 import Mathlib.RingTheory.DiscreteValuationRing.TFAE
 import Mathlib.FieldTheory.PrimitiveElement
@@ -33,21 +35,23 @@ def rankExtDVR : ℕ := (ramificationIdx A B) * (inertiaDeg A B)
 variable [Module.Finite A B] [IsSeparable (ResidueField A) (ResidueField B)]
 
 instance instFiniteExtResidue : FiniteDimensional (ResidueField A) (ResidueField B) := sorry
--- variable (hker : RingHom.ker (algebraMap A B) < maximalIdeal A)
 
 open IntermediateField Polynomial
 
 -- `x` : a lift of a primitive element of `k_B/k_A`
 -- WARNING: `hx` might not be a good statement
-variable {x : B} (hx : (ResidueField A)⟮residue B x⟯= ⊤)
+variable {x : B} (hx : (ResidueField A)⟮residue B x⟯ = ⊤)
 -- `f` : a monic polynomial with `A`-coeff that reduce to the minpoly of `x : k_B` over `k_A`
 variable {f : A[X]} (h_monic : Monic f) (h_red : f.map (residue A) = minpoly (ResidueField A) (residue B x))
+-- `ϖ` : a uniformizer `ϖ` of `B`
+variable {ϖ : B} (hϖ : Irreducible ϖ)
 
 theorem fx_ne_0 : f.eval₂ (algebraMap A B) x ≠ 0 := sorry
 
--- `ϖ` : an uniformizer `ϖ` of `B`
-variable {ϖ : B} (hϖ : Irreducible ϖ)
-
+/-some possibly useful thms are listed below-/
+#check Field.powerBasisOfFiniteOfSeparable (ResidueField A) (ResidueField B)
+#check Algebra.adjoin.powerBasis'
+#check Algebra.IsIntegral.of_finite
 /-
 lemma 4 states that the lifting `x` could be choose so that `f x` is a uniformizer of `B`.
 Proof:
@@ -59,12 +63,16 @@ The following thm is the `otherwise` case.
 
 theorem lemma4_val_ge_2 (h_fx : ¬Irreducible (f.eval₂ (algebraMap A B) x)) : Irreducible (f.eval₂ (algebraMap A B) (x + ϖ)) := sorry
 
+/-
+The following two theorem states that `B = A[x]` if `f x` is a uniformizer;
+otherwise `B = A[x + ϖ]`.
+-/
+theorem thm_val_1 (h_fx : Irreducible (f.eval₂ (algebraMap A B) x)) : Algebra.adjoin A {x} = ⊤ := sorry
 
-#check Field.powerBasisOfFiniteOfSeparable (ResidueField A) (ResidueField B)
+theorem thm_val_ge_2 (h_fx : ¬Irreducible (f.eval₂ (algebraMap A B) x)) : Algebra.adjoin A {x + ϖ} = ⊤ := sorry
+
+
 
 def PowerBasisExtDVR : PowerBasis A B := sorry
-
-#check Algebra.adjoin.powerBasis'
-#check Algebra.IsIntegral.of_finite
 
 end ExtDVR
