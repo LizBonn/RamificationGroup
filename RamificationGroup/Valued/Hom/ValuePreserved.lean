d6aca06fbe6e2890eb441e6d1ef76152b935667f
Prowler99 <leibichang58@ruc.edu.cn>
2024-03-28 20:48:41 +0800

    formulate a possibly true result

--- RamificationGroup/Valued/Hom/ValuePreserved.lean
+++ RamificationGroup/Valued/Hom/ValuePreserved.lean
@@ -1,10 +1,90 @@
-import RamificationGroup.Valued.Hom.Defs
-
-variable {K L : Type*} {ΓK: outParam Type*} [Ring K] [Ring L]
-  [LinearOrderedCommGroupWithZero ΓK] [vK : Valued K ΓK]
-variable {f : K →+* L}
-
 /-
-1. how to add completeness
-2. should be parametrized over `ValRingHomClass`, right?
+Might need to use another namespace
 -/
+
+import RamificationGroup.Valued.Hom.Defs
+import LocalClassFieldTheory.DiscreteValuationRing.Extensions
+
+variable {K : Type*} {ΓK : outParam Type*} [Field K]
+  [LinearOrderedCommGroupWithZero ΓK] [vK : Valued K ΓK]
+  [CompleteSpace K]
+variable {L : Type*} [Field L]
+
+namespace Valuation
+
+variable [Algebra K L] [FiniteDimensional K L]
+
+section int_closure
+
+#check DiscreteValuation.Extension.integralClosure_eq_integer
+
+variable {Γ : outParam Type*} [LinearOrderedCommGroupWithZero Γ]
+  {v : Valuation L Γ}
+
+theorem integral_closure_eq_integer
+  (h : vK.v.IsEquiv <| v.comap (algebraMap K L)) :
+    (integralClosure vK.v.valuationSubring L).toSubring = v.integer := by
+  sorry
+
+end int_closure
+
+section value_ext
+
+variable [Algebra K L] [FiniteDimensional K L]
+variable {Γ₁ Γ₂ : outParam Type*} [LinearOrderedCommGroupWithZero Γ₁] [LinearOrderedCommGroupWithZero Γ₂]
+  {v₁ : Valuation L Γ₁} {v₂ : Valuation L Γ₂}
+
+theorem unique_valuationSubring_of_ext (h₁ : vK.v.IsEquiv <| v₁.comap (algebraMap K L))
+  (h₂ : vK.v.IsEquiv <| v₂.comap (algebraMap K L)) :
+    v₁.valuationSubring = v₂.valuationSubring := by
+  ext
+  rw [Valuation.mem_valuationSubring_iff, Valuation.mem_valuationSubring_iff,
+    ← Valuation.mem_integer_iff, ← Valuation.mem_integer_iff,
+    ← integral_closure_eq_integer h₁, ← integral_closure_eq_integer h₂]
+
+theorem unique_val_of_ext (h₁ : vK.v.IsEquiv <| v₁.comap (algebraMap K L))
+  (h₂ : vK.v.IsEquiv <| v₂.comap (algebraMap K L)) :
+    v₁.IsEquiv v₂ :=
+  (Valuation.isEquiv_iff_valuationSubring _ _).mpr <| unique_valuationSubring_of_ext h₁ h₂
+
+
+end value_ext
+
+end Valuation
+
+namespace ValAlgEquiv
+
+section alg_end
+variable {ΓL : outParam Type*} [LinearOrderedCommGroupWithZero ΓL] [vL : Valued L ΓL]
+variable [ValAlgebra K L] [FiniteDimensional K L]
+
+theorem algEnd_preserve_val (f : L →ₐ[K] L) : vL.v.IsEquiv <| vL.v.comap f := by
+  apply Valuation.unique_val_of_ext (K := K)
+  · apply ValAlgebra.val_isEquiv_comap
+  · rw [Valuation.isEquiv_iff_val_le_one]
+    simp; intro x
+    rw [← Valuation.comap_apply (algebraMap K L)]
+    revert x
+    rw [← Valuation.isEquiv_iff_val_le_one]
+    apply ValAlgebra.val_isEquiv_comap
+
+
+theorem algEquiv_preserve_val (f : L ≃ₐ[K] L) : vL.v.IsEquiv <| vL.v.comap f := algEnd_preserve_val f.toAlgHom
+
+variable (K L) in
+def fromAlgEquiv : (L ≃ₐ[K] L) →* (L ≃ₐv[K] L) where
+  toFun := fun f ↦ mk' f <| algEquiv_preserve_val f
+  map_one' := rfl
+  map_mul' := sorry
+
+variable (K L) in
+def equivAlgEquiv : (L ≃ₐ[K] L) ≃* (L ≃ₐv[K] L) where
+  toFun := fromAlgEquiv K L
+  invFun := toAlgEquiv
+  left_inv := sorry
+  right_inv := sorry
+  map_mul' := sorry
+
+end alg_end
+
+end ValAlgEquiv
