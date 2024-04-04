a554dd91fbc100f2f027e4d6c03977c84ee34079
Prowler99 <leibichang58@ruc.edu.cn>
2024-04-04 20:35:01 +0800

    start to prove approximation lemma

--- RamificationGroup/Valuation/Discrete.lean
+++ RamificationGroup/Valuation/Discrete.lean
@@ -3,19 +3,57 @@ import LocalClassFieldTheory.DiscreteValuationRing.DiscreteNorm
 import RamificationGroup.ForMathlib.Henselian
 import RamificationGroup.Valued.Hom.Defs
 
-#check Valued.mem_nhds_zero
+
 
 open Valuation Valued DiscreteValuation
+
+#check IsRankOne
+#check IsDiscrete
+#check DiscreteValuation.isRankOne
+
+
+section approximation
+
+namespace DiscreteValuation
+
+open IsRankOne
+open scoped NNReal
+
+variable {K : Type*} [Field K]
+  -- {ΓK ΓK' : outParam Type*}
+  -- [LinearOrderedCommGroupWithZero ΓK] [LinearOrderedCommGroupWithZero ΓK']
+  -- {v₁ : Valuation K ΓK} [IsRankOne v₁]
+  -- {v' : Valuation K ΓK'} [IsRankOne v']
+  {v v' : Valuation K ℤₘ₀} [IsDiscrete v] [IsDiscrete v']
+
+-- theorem isEquiv_iff_eq_pow : v.IsEquiv v' ↔ ∃s : ℝ≥0, ∀x : K, hom v (v x) = ((hom v' (v' x)) ^ (s : Real)) := by
+--   sorry
+
+-- #check pow_Uniformizer
+-- theorem val_pow_Uniformizer : sorry := sorry
+
+theorem isEquiv_of_le_one_le_one_of_eq_one_eq_one
+    (h : ∀x : K, v x ≤ 1 → v' x ≤ 1) (h1 : ∀x : K, v x = 1 → v' x = 1) :
+  v.IsEquiv v' := by
+  sorry
+
+theorem isEquiv_of_le_one_le_one (h : ∀x : K, v x ≤ 1 → v' x ≤ 1) :
+  v.IsEquiv v' := by
+  -- rcases exists_Uniformizer_ofDiscrete v with ⟨π, hπ⟩
+
+  sorry
+
+
+end DiscreteValuation
+
+end approximation
+
+
+section adic_complete
 
 variable {K : Type*} [Field K] [vK : Valued K ℤₘ₀]
 
 local notation "m[" K "]" => LocalRing.maximalIdeal 𝒪[K]
-
-namespace DiscreteValuation
-
-#synth IsFractionRing 𝒪[K] K
-
-section adic_complete
 
 variable [CompleteSpace K] [IsDiscrete vK.v]
 
