43da03c8dafb4a1cc87b3b089a5be32fbff732ff
Prowler99 <leibichang58@ruc.edu.cn>
2024-04-11 17:17:04 +0800

    Update Discrete.lean

--- RamificationGroup/Valuation/Discrete.lean
+++ RamificationGroup/Valuation/Discrete.lean
@@ -1,3 +1,7 @@
+/-
+# WARNING : `WithZero.lean` uses `sorry`
+-/
+
 import LocalClassFieldTheory.DiscreteValuationRing.Complete
 import LocalClassFieldTheory.DiscreteValuationRing.DiscreteNorm
 import RamificationGroup.ForMathlib.Henselian
@@ -11,11 +15,73 @@ open Valuation Valued DiscreteValuation Multiplicative
 #check IsDiscrete
 #check DiscreteValuation.isRankOne
 
+#check Int.eq_one_of_dvd_one
+
+theorem Int.eq_neg_one_of_dvd_neg_one {x : ℤ} (h : x ≤ 0) (h' : x ∣ -1) : x = -1 := by
+  let y := -x
+  simp only [show x = -y by rw [neg_neg], Left.neg_nonpos_iff, reduceNeg, dvd_neg, neg_dvd, neg_inj] at *
+  exact Int.eq_one_of_dvd_one h h'
+
+
+#check dvd_neg
+#check Int.eq_one_of_dvd_one
 #check ℤₘ₀
+namespace WithZero
 
-theorem WithZero.ofAdd_eq_neg_one_of_pow_eq_neg_one {x : ℤₘ₀} (h1 : x ≤ 1) {n : ℤ} (hn : x ^ n = ofAdd (-1 : ℤ)) : x = ofAdd (-1 : ℤ) := by
-  sorry
+#check congrArg
+#check ofAdd_eq_one
+#check unzero
+#check WithZero.coe_inj
 
+#synth GroupWithZero ℤₘ₀
+
+
+theorem ofAdd_eq_neg_one_of_pow_eq_neg_one {x : ℤₘ₀} (h1 : x ≤ 1) {n : ℤ} (hn : x ^ n = ofAdd (-1 : ℤ)) : x = ofAdd (-1 : ℤ) := by
+  by_cases hn0 : n = 0
+  · simp only [hn0, zpow_zero, Int.reduceNeg, ofAdd_neg, coe_inv, one_eq_inv] at hn
+    contradiction
+  · match x with
+    | 0 =>
+      simp at *
+      rw [zero_zpow _ hn0] at hn
+      contradiction
+    | .coe (.ofAdd a)  =>
+      change ((ofAdd (n * a) : Multiplicative ℤ) : ℤₘ₀) = (ofAdd (-1 : ℤ)) at hn
+      change ↑(ofAdd a) ≤ (((ofAdd (0:ℤ)): Multiplicative ℤ) : ℤₘ₀) at h1
+      rw [coe_le_coe, Multiplicative.ofAdd_le] at h1
+      rw [coe_inj] at *
+      show a = -1
+      change n * a = -1 at hn
+      apply Int.eq_neg_one_of_dvd_neg_one h1 (dvd_of_mul_left_eq _ hn)
+
+/-
+  have nne0 : n ≠ 0 := by
+    intro a
+    simp only [a, zpow_zero, Int.reduceNeg, ofAdd_neg, coe_inv, one_eq_inv] at hn
+    rw [show (1 : ℤₘ₀) = ((1 : Multiplicative ℤ) : ℤₘ₀) by rfl, coe_inj] at hn
+    have : ofAdd (1 : ℤ) ≠ (1 : Multiplicative ℤ) := by
+      simp only [ne_eq, ofAdd_eq_one, one_ne_zero, not_false_eq_true]
+    exact this hn
+  have xne0 : x ≠ 0 := by
+    intro a
+    rw [a, zero_zpow n nne0] at hn
+    revert hn
+    simp only [Int.reduceNeg, ofAdd_neg, coe_inv, zero_eq_inv, zero_ne_coe, IsEmpty.forall_iff]
+  rw [← coe_unzero xne0, ← coe_zpow, coe_inj] at hn
+  rw [← coe_unzero xne0, coe_inj]
+  rw [show (1 : ℤₘ₀) = ofAdd (0 : ℤ) by rfl] at h1
+  rw [← coe_unzero xne0, coe_le_coe] at h1
+  have : unzero xne0 = ofAdd (toAdd (unzero xne0)) := (ofAdd_toAdd _).symm
+  rw [this, Multiplicative.ofAdd_le] at h1
+  rw [this, ← ofAdd_zsmul] at hn
+  replace hn := ofAdd_inj hn
+  simp only [smul_eq_mul, Int.reduceNeg] at hn
+  rw [this]
+  congr 1
+  apply Int.eq_neg_one_of_dvd_neg_one h1 (dvd_of_mul_left_eq _ hn)
+-/
+
+end WithZero
 section approximation
 
 namespace Valuation
@@ -35,6 +101,13 @@ lemma eq_one_of_eq_one_of_le_one_le_one (h : ∀{x : K}, v x ≤ 1 → v' x ≤ 
     rw [← inv_le_one₀ this, ← map_inv₀]
     apply h <| le_of_eq _
     rw [map_inv₀, hu, inv_one]
+
+variable (v) in
+theorem map_zpow (x : K) (n : ℤ) :
+  v (x ^ n) = v x ^ n := by
+  match n with
+  | .ofNat a => simp only [Int.ofNat_eq_coe, zpow_coe_nat, _root_.map_pow]
+  | .negSucc a => simp only [zpow_negSucc, map_inv₀, _root_.map_pow]
 
 end division_ring
 
@@ -115,20 +188,20 @@ theorem val_pow_Uniformizer {π : Uniformizer v} {n : ℕ} {u : v.valuationSubri
   congr 1
   simp only [Int.reduceNeg, ofAdd_neg, inv_pow, ← ofAdd_nsmul, nsmul_eq_mul, mul_one]
 
--- TODO: `map_pow` for `ℤ`-pow
+-- TODO: `map_zpow` for `ℤ`-pow
 theorem val_pow_Uniformizer_all {π : Uniformizer v} {n : ℤ} {u : v.valuationSubringˣ} : v ((π.1 : K) ^ n * u.1) = Multiplicative.ofAdd (-n : ℤ) := by
-  sorry
+  rw [v.map_mul, Valuation.map_zpow, π.2, val_valuationSubring_unit, mul_one, ← WithZero.coe_zpow]
+  congr 1
+  change n * -1 = -n
+  exact mul_neg_one n
 
+/--If `π : K` is a uniformizer for `v`, and `v x ≤ 1 → v' x ≤ 1 ∀x : K`, then `π` is also a uniformizer for `v'`.-/
 lemma isUniformizer_of_uniformizer_of_le_one_le_one (h : ∀{x : K}, v x ≤ 1 → v' x ≤ 1) (π : Uniformizer v) : IsUniformizer v' π.1 := by
   rcases exists_Uniformizer_ofDiscrete v' with ⟨π', hπ'⟩
   rcases pow_Uniformizer_all (Uniformizer_ne_zero v' hπ') π with ⟨m, u, hmu⟩
   replace hmu := congrArg v' hmu
   rw [_root_.map_mul, map_zpow₀,
-    eq_one_of_eq_one_of_le_one_le_one h (val_valuationSubring_unit (u := u)), mul_one, hπ'] at hmu
-  -- have : v' π.1 = ofAdd (-1 : ℤ) := by
-  --   apply WithZero.ofAdd_eq_neg_one_of_pow_eq_neg_one _ (h <| le_of_lt <| Uniformizer_valuation_lt_one v π.2) hmu.symm
-  --   rw [Valuation.ne_zero_iff]
-  --   exact Uniformizer_ne_zero' v π
+    eq_one_of_eq_one_of_le_one_le_one h val_valuationSubring_unit, mul_one, hπ'] at hmu
   rw [IsUniformizer, WithZero.ofAdd_eq_neg_one_of_pow_eq_neg_one (h <| le_of_lt <| Uniformizer_valuation_lt_one v π.2) hmu.symm]
 
 /-- Two discrete valuations `v` and `v'` on a field `K` are equivalent, if `v x ≤ 1 → v' x ≤ 1, ∀x : K`. -/
@@ -138,15 +211,13 @@ theorem isEquiv_of_le_one_le_one (h : ∀{x : K}, v x ≤ 1 → v' x ≤ 1) :
   refine fun {x} ↦ ⟨h, ?_⟩
   by_cases xne0 : x = 0
   · intro
-    rw [show v x = 0 by rw[xne0, Valuation.map_zero]]
+    rw [xne0, Valuation.map_zero]
     exact zero_le_one
   intro v'xle
-  rcases exists_Uniformizer_ofDiscrete v' with ⟨π, hπ⟩
-  rcases pow_Uniformizer' xne0 v'xle ⟨π, hπ⟩ with ⟨n, u, hnu⟩
-  simp only [SubmonoidClass.coe_pow] at hnu
-  rw [hnu]
-
-
+  -- rcases exists_Uniformizer_ofDiscrete v' with ⟨π, hπ⟩
+  -- rcases pow_Uniformizer' xne0 v'xle ⟨π, hπ⟩ with ⟨n, u, hnu⟩
+  -- simp only [SubmonoidClass.coe_pow] at hnu
+  -- rw [hnu]
 
   sorry
 
