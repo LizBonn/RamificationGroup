31b6a522ebced67b10b2f0876c6f6d92c6a644cf
timechess <tyc221@ruc.edu.cn>
2024-04-24 17:19:12 +0800

    Blueprint (#1)
    * blueprint
    
    * fix url

--- lakefile.lean
+++ lakefile.lean
@@ -9,13 +9,14 @@ package «RamificationGroup» where
     ⟨`relaxedAutoImplicit, false⟩
   ]
 
+@[default_target]
 lean_lib «RamificationGroup» where
   -- add library configuration options here
 
-@[default_target]
-lean_exe «ramificationgroup» where
-  root := `Main
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
 
 require local_class_field_theory from git "https://github.com/mariainesdff/LocalClassFieldTheory.git"
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
