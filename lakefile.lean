073dee2eda73da4cbcc064aa803f71906def07f2
Yicheng Tao <tyc221@ruc.edu.cn>
2024-12-08 22:29:59 +0800

    fix blueprint (#8)

--- lakefile.lean
+++ lakefile.lean
@@ -19,4 +19,4 @@ lean_lib «RamificationGroup» where
 require local_class_field_theory from git "https://github.com/mariainesdff/LocalClassFieldTheory.git"
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "c7f4ac84b973b6efd8f24ba2b006cad1b32c9c53"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "v4.13.0"
